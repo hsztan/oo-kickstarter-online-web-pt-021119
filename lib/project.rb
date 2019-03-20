cc0c7d4785b13375d203e15999ae159f513171de
Henry Sztanski <henry@sztanski.com>
2019-03-20 19:38:23 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,11 +1,13 @@
 class Project
-  attr_reader :backers
+  attr_reader :name, :backers
 
-  def initialize
+  def initialize(name)
+    @name = name
     @backers = []
   end
 
   def add_backer(backer)
     @backers << backer if backer.is_a(Backer)
+    backer.back_project(self)
   end
 end
