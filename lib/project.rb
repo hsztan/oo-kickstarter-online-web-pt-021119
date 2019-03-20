ba894552e523b6327f87d007eda6610249bedad6
Henry Sztanski <henry@sztanski.com>
2019-03-20 19:47:16 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -8,6 +8,6 @@ class Project
 
   def add_backer(backer)
     @backers << backer if backer.is_a?(Backer)
-    backer.back_project(self)
+    backer.back_project(self) unless backer.backed_projects.include?(self)
   end
 end
